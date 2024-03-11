5b57f98554f4774c162a1a9b8cb960944d49375b
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2024-03-11 13:01:51 +0000

    conversion of data types

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score="33abc"
+console.log(typeof score)
+
+let valueofnumber=Number(score)
+console.log(typeof valueofnumber)
+console.log(valueofnumber) //NAN means not a number but typeof says it is a number
+
+// "33"=. 33
+//"33abc"=> nan
+//true=>1; false=>0
+
+let newbool=0
+change=Boolean(newbool)
+console.log(typeof change)
+console.log(change)
+
+// 1=> true ; 0=> false
+//""=> false
+//"jay"=> true 
+
+let somenumber = 33
+let stringnumber= String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
