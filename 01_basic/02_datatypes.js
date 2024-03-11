814aaabe657452bc8d5ce7e66015331d5203c496
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2024-03-11 12:25:13 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"user strict";// treat all JS code as newer version
+//alert (3+3) // we are using nodejs , not browser
+
+console.log(3
+    +3) //code  readability should be high
+console.log("amanjay")
+
+let name="jay"
+let age = 23
+let isloggedin=false
+
+
+//number= 2 to power 53 (range)
+//bigint
+//string=""
+//boolean=false/true
+//null=standalone value
+// symbol= unique
+
+//object 
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
