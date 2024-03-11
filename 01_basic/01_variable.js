b570de3ff36b166c370583852ff0a3730d23118c
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2024-03-11 12:27:30 +0000

    variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 16210051
+let accountEmail="jay@gmail.com"
+var accountPass="9090909"
+accountCity="chennai"
+let accountState;
+
+/* DONOT USE VAR  BECAUSE OF ISSUE IN BLOCK CODE AND FUNCTIONAL SCOPE*/
+accountEmail="arnav@gmail.com"
+accountPass="1211212"
+accountCity="bhopal"
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
