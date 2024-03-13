4a26220b4e424759b7a322729b3837d23f8163b8
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2024-03-13 09:23:28 +0000

    operation

---
+++ 01_basic/04_opeartion.js
@@ -0,0 +1,14 @@
+let value=3
+let negvalue= - value
+console.log(negvalue)
+
+console.log("1"+2);//12
+console.log(1+"2")//12
+console.log("1"+2+2)// 122
+console.log(1+2+"2")//32 **
+
+
+let num=3
+const new_num=++num // num++ => num chamged to 4 ; new_num =3 
+console.log(num)// 4
+console.log(new_num) // 4
