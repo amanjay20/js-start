69d66688203b34eff2dc17d0b33472d63b13532e
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2024-04-01 12:27:14 +0000

    destructure

---
+++ 02_basics/05_de_structure.js
@@ -0,0 +1,10 @@
+const course={
+    courseName:"js in hindi",
+    courseInstructor:"Hiteshchoudhary",
+    fees:999
+
+
+}
+
+const {courseInstructor:Instructor}=course// rewriting course.courseinstructior we used this method 
+console.log(Instructor);
