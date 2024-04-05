d2f14d40a4b6d1099a5ac1d4ca7133359b778a0a
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2024-04-05 06:42:50 +0000

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,39 @@
+const month = 3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default exceuted");
+        break;
+}
+
+const name="as"
+switch (name) {
+    case "ar":
+        console.log("Arnav");   
+        break;
+    case "an":
+        console.log("Anil");   
+        break;
+    case "am":
+        console.log("Amit");   
+        break;
+    case "aj":
+        console.log("Amanjay");   
+        break;
+
+    default:
+        console.log("give correct input");
+        break;
+}
