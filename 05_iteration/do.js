03672d987681320a76a6b42813f84b8da9e56734
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2024-04-05 13:14:58 +0000

    DO AND DO WHILE

---
+++ 05_iteration/do.js
@@ -0,0 +1,20 @@
+let index=0
+while (index<=10) {
+    console.log(`value of index  is ${index}`);
+    index=index+2
+
+}
+
+let myarray=["jay","aman","arnav"]
+let start=0
+while (start<myarray.length) {
+    console.log(`value in myarray is ${myarray[start]}`);
+    start=start+1
+    
+}
+//DO WHILE SNTAX 
+let score =0
+do {
+    console.log(`my score is ${score}`);
+    score++
+} while (score<=10);// if we give score =11 then one line print my score is 11 and after that nothing print.
